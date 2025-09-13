e90cc7d525358bea66d7a8902e57d49092a3cbda
Nathan Garside <git@nathangarside.com>
2025-09-13 15:17:26 +0100

    Compress output files

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@
 # Setup environment variables.
 export ARCH=$(uname -m)
 export CXX="zig c++ -w"
+export DUCKDB="1.3.2"
 export LIB="$(dirname $BASH_SOURCE[0])/../lib"
 export PATH="/opt/zig:$PATH"
 export ZIG="0.15.1"
@@ -14,10 +15,13 @@ tar --directory /opt/zig --extract --file /tmp/zig.tar.xz --strip-components 1 -
 rm /tmp/zig.tar.xz
 
 # Clone & build duckdb.
-git clone --branch v1.3.2 --single-branch https://github.com/duckdb/duckdb /opt/duckdb
+git clone --branch v$DUCKDB --depth 1 https://github.com/duckdb/duckdb /opt/duckdb
 (cd /opt/duckdb && make bundle-library)
 
 # Copy compile output to 'lib' directory.
-mkdir --parents $LIB
-cp /opt/duckdb/src/include/duckdb.h $LIB/duckdb.h
-cp /opt/duckdb/build/release/libduckdb_bundle.a $LIB/libduckdb.a
+mkdir --parents $LIB/bin
+cp /opt/duckdb/src/include/duckdb.h $LIB/bin/duckdb.h
+cp /opt/duckdb/build/release/libduckdb_bundle.a $LIB/bin/libduckdb.a
+
+# Compress output files.
+zip --junk-paths $LIB/duckdb.zip $LIB/bin/*
