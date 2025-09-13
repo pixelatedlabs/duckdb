4fd13013669ccab23cb4d0fd2353bae8f9780a70
Nathan Garside <git@nathangarside.com>
2025-09-13 16:30:55 +0100

    Dynamically name archive (#4)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,12 @@
 # This is free and unencumbered software released into the public domain.
 
+set pipefail -euo
+
 # Setup environment variables.
 export ARCH=$(uname -m)
 export CXX="zig c++ -w"
 export LIB="$(dirname $BASH_SOURCE[0])/../lib"
 export PATH="/opt/zig:$PATH"
-export ZIG="0.15.1"
 
 # Install zig.
 mkdir /opt/zig
@@ -14,10 +15,18 @@ tar --directory /opt/zig --extract --file /tmp/zig.tar.xz --strip-components 1 -
 rm /tmp/zig.tar.xz
 
 # Clone & build duckdb.
-git clone --branch v1.3.2 --single-branch https://github.com/duckdb/duckdb /opt/duckdb
+git clone --branch v$DUCKDB --depth 1 https://github.com/duckdb/duckdb /opt/duckdb
 (cd /opt/duckdb && make bundle-library)
 
 # Copy compile output to 'lib' directory.
-mkdir --parents $LIB
-cp /opt/duckdb/src/include/duckdb.h $LIB/duckdb.h
-cp /opt/duckdb/build/release/libduckdb_bundle.a $LIB/libduckdb.a
+mkdir --parents $LIB/bin
+cp /opt/duckdb/src/include/duckdb.h $LIB/bin/duckdb.h
+cp /opt/duckdb/build/release/libduckdb_bundle.a $LIB/bin/libduckdb.a
+
+# Compress output files.
+case $ARCH in
+	"aarch64") EXT="arm64" ;;
+	"x86_64") EXT="x64" ;;
+	*) EXT=$ARCH ;;
+esac
+zip --junk-paths $LIB/duckdb-$ZIG-$DUCKDB-$EXT.zip $LIB/bin/*
