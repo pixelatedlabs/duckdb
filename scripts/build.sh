f0eac4667f3d959677a4f2c399bb430dfcfeb11c
Nathan Garside <git@nathangarside.com>
2025-09-13 14:26:52 +0100

    Add local build support (#1)

---
+++ scripts/build.sh
@@ -0,0 +1,23 @@
+# This is free and unencumbered software released into the public domain.
+
+# Setup environment variables.
+export ARCH=$(uname -m)
+export CXX="zig c++ -w"
+export LIB="$(dirname $BASH_SOURCE[0])/../lib"
+export PATH="/opt/zig:$PATH"
+export ZIG="0.15.1"
+
+# Install zig.
+mkdir /opt/zig
+curl --output /tmp/zig.tar.xz https://ziglang.org/download/$ZIG/zig-$ARCH-linux-$ZIG.tar.xz
+tar --directory /opt/zig --extract --file /tmp/zig.tar.xz --strip-components 1 --xz
+rm /tmp/zig.tar.xz
+
+# Clone & build duckdb.
+git clone --branch v1.3.2 --single-branch https://github.com/duckdb/duckdb /opt/duckdb
+(cd /opt/duckdb && make bundle-library)
+
+# Copy compile output to 'lib' directory.
+mkdir --parents $LIB
+cp /opt/duckdb/src/include/duckdb.h $LIB/duckdb.h
+cp /opt/duckdb/build/release/libduckdb_bundle.a $LIB/libduckdb.a
